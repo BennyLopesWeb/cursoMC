9bd642c4aabab2904ccf79eb218a78be7ccb6eef
Benny Lopes <benignofernandeslopes@gmail.com>
2021-11-26 01:52:30 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/benigno/cursoMC/domain/Categoria.java
+++ src/main/java/com/benigno/cursoMC/domain/Categoria.java
@@ -3,6 +3,12 @@ package com.benigno.cursoMC.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	/**
@@ -10,7 +16,10 @@ public class Categoria implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY )
 	private Integer Id;
+	
 	private String Nome;
 	
 	
